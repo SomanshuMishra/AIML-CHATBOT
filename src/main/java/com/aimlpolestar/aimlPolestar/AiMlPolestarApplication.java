ed1ff57a6040e49997a831eb605e62b3806dd290
Somanshu Mishra <mishrasomanshu@gmail.com>
2024-04-06 14:35:40 +0530

    added first class

--- src/main/java/com/aimlpolestar/aimlPolestar/AiMlPolestarApplication.java
+++ src/main/java/com/aimlpolestar/aimlPolestar/AiMlPolestarApplication.java
@@ -2,12 +2,21 @@ package com.aimlpolestar.aimlPolestar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AiMlPolestarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiMlPolestarApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/")
+	public String hello() {
+	   return "Welcome to AIML Vaiday Backend Universe";
 	}
 
 }
